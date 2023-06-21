8af1a03b40d3af0de0214bbf736c15d46f46ca31
InessaMusiienko <135662923+InessaMusiienko@users.noreply.github.com>
2023-06-21 15:04:20 +0300

    Add new controller, view model with data, action for news

---
+++ CinemaWorld/Controllers/NewsController.cs
@@ -0,0 +1,16 @@
+﻿using CinemaWorld.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaWorld.Controllers
+{
+    public class NewsController : BaseController
+    {
+        [AllowAnonymous]
+        //public async Task<IActionResult> News()
+        //{
+        //    var model = await filmService.GetAllFilmsAsync();
+        //    return View(model);
+        //}
+    }
+}
---
+++ CinemaWorld/Models/NewsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CinemaWorld.Models
+{
+    public class NewsViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public DateTime Date { get; set; }
+        public string PhotoUrl { get; set; } = null!;
+    }
+}
