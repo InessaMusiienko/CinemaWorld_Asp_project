06ca7eae81fa86a8d6d6e51df61f1c95f4613dae
InessaMusiienko <135662923+InessaMusiienko@users.noreply.github.com>
2023-08-07 18:01:05 +0300

    Fix bugs (comment section)

--- CinemaWorld/wwwroot/js/likeBtn.js
+++ CinemaWorld/wwwroot/js/likeBtn.js
@@ -1,8 +1,23 @@
-﻿
-const thumb = document.getElementById('likeDislike');
-thumb.onclick=toggleLikes;
-function toggleLikes() {
-    const icon = document.getElementById('thumbIcon');
-    icon.classList.toggle('fa-thumbs-up');
-    icon.classList.toggle('fa-thumbs-down');  
+﻿function solve(name, population, treasure){
+    let city = {
+        name,
+        population, 
+        treasure,
+        taxRate: 10,
+        collectTaxes() {
+            this.treasure += this.population * this.taxRate;
+        },
+        applyGrowth(percentage) {
+            this.population += Math.floor(this.population * percentage / 100);
+        },
+        applyRecession(percentage) {
+            this.treasure -= Math.floor(this.treasure * percentage / 100);
+        }
+    }
+    return city;
 }
+
+let city1 = solve('Tortuga', 7000, 15000);
+console.log(city1);
+city1.collectTaxes();
+console.log(city1);
